5bfbdb3e40560e95de6824c8f616a0fc13fa0da2
ostwind <vinnikove21@gmail.com>
2022-12-10 14:44:55 +0300

    add material ui and global.css

--- hack2022-frontend/src/Components/CarInfo/CarInfo.jsx
+++ hack2022-frontend/src/Components/CarInfo/CarInfo.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const CarInfo = () => {
-  return <div>CarInfo</div>;
+  return <div>
+    <div>
+        <video></video>
+        <div>
+          Фото чела
+        </div>
+        <div>
+          Инфо машины
+        </div>
+    </div>
+  </div>;
 };
 
 export default CarInfo;
