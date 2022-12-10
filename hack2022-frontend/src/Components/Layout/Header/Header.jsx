eabb2a4acdd66afbd20fc998be3f6a8097c74cbd
Misha-dev <shabatinmichael@mail.ru>
2022-12-11 01:30:44 +0500

    added logic for logout button in header component

--- hack2022-frontend/src/Components/Layout/Header/Header.jsx
+++ hack2022-frontend/src/Components/Layout/Header/Header.jsx
@@ -1,15 +1,31 @@
 // @ts-nocheck
 import { Button } from "@mui/material";
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectUser } from "../../../store/userSlice";
 import cl from "./Header.module.scss";
 const Header = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
   return (
     <header className={cl.wrapper}>
       <img className={cl.logo} src={require("../../../imgs/logo.png")} alt="logo" />
       <h1>CCTV</h1>
-      <Button sx={{ fontWeight: "1000" }} variant="outlined" color="error">
-        Logout
-      </Button>
+      {user.login ? (
+        <Button
+          sx={{ fontWeight: "1000" }}
+          variant="outlined"
+          color="error"
+          onClick={() => {
+            localStorage.clear();
+            dispatch(logout());
+          }}
+        >
+          Logout
+        </Button>
+      ) : (
+        <></>
+      )}
     </header>
   );
 };
