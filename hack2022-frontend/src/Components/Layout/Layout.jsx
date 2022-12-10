f91e8bd33585dead8acc17a47896516f0400d17b
Misha-dev <shabatinmichael@mail.ru>
2022-12-10 15:49:43 +0500

    added router for navigation

---
+++ hack2022-frontend/src/Components/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+      <header>Header</header>
+      {<Outlet />}
+      <footer>Footer</footer>
+    </div>
+  );
+};
+
+export default Layout;
