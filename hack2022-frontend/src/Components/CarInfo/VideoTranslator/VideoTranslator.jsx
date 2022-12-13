22cbe96fe18472389b7e4d1a06488b2cb273eca8
Misha-dev <shabatinmichael@mail.ru>
2022-12-14 00:44:22 +0500

    imroved syntax of writing classes in VideoTranslator component

--- hack2022-frontend/src/Components/CarInfo/VideoTranslator/VideoTranslator.jsx
+++ hack2022-frontend/src/Components/CarInfo/VideoTranslator/VideoTranslator.jsx
@@ -1,13 +1,15 @@
 // @ts-nocheck
 import { Box, CircularProgress } from "@mui/material";
+import cn from "classnames";
 import React, { useState } from "react";
 import { CardBox } from "../../CardBox/CardBox";
 import styles from "./VideoTranslator.module.scss";
 
 export const VideoTranslator = ({ videoName }) => {
   const [isClicked, setIsClicked] = useState(false);
+  const classes = cn(styles.camera__item, { [styles.camera__item_clicked]: isClicked });
   return (
-    <CardBox className={`${styles.camera__item} ${isClicked ? styles.camera__item_clicked : ""}`}>
+    <CardBox className={cn(styles.camera__item, { [styles.camera__item_clicked]: isClicked })}>
       <div onClick={() => setIsClicked(!isClicked)} width={"100%"} height={"calc(100% /2)"} style={{ position: "relative" }}>
         <video style={{ position: "relative", zIndex: 1 }} preload="auto" autoPlay={true} width={"100%"} height={"100%"} muted>
           <source src={require(`../../../assets/video/${videoName}`)} type="video/mp4" />
